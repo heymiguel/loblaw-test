c2db04e2668982205c268565f5846722e59c37d2
heymiguel <miguelcesarbautista@gmail.com>
2017-03-02 06:05:20 -0500

    show more proof of concept

--- src/ProductList.js
+++ src/ProductList.js
@@ -5,35 +5,69 @@ import axios from 'axios';
 class ProductList extends Component {
   constructor( props ) {
     super( props );
+    this.initialFilter = this.initialFilter.bind( this );
+    this.showMore = this.showMore.bind( this );
     this.state = {
       productList: [],
-      displayList: []
+      displayList: [],
+      index: {
+        start: 0,
+        end: 20
+      }
     };
   }
   render() {
+
     return (
-      <section className="product-list">
-        <ul>
-          {this.state.productList.map(( product ) => {
-            return (
-              <Product
-                key={product.productId}
-                name={product.productName}
-                color={product.productColor}
-                price={product.productPrice}
-                badgeString={product.productBadgeString}
-                thumbnail={product.thumbnails.b2}
-                altText={product.productName}
-              ></Product>
-            )
-          })}
-        </ul>
+      <div>  
         <div className="filters">
-          <button className="a-to-z">A to Z</button>
-          <button className="by-price">by Price</button>
+            <button className="a-to-z">A to Z</button>
+            <button className="by-price">by Price</button>
         </div>
-      </section>
+        <section className="product-list">
+          <ul>
+            {this.state.displayList.map(( product,index ) => {
+              return (
+                <Product
+                  key={index}
+                  name={product.productName}
+                  color={product.productColor}
+                  price={product.productPrice}
+                  badgeString={product.productBadgeString}
+                  thumbnail={product.thumbnails.b2}
+                ></Product>
+              )
+            })}
+          </ul>
+          <button className="show-more" onClick={this.showMore}> show more </button>
+          </section>
+      </div>
     );
+  }
+
+  initialFilter( arrayToFilter ) {
+    let tempArray = arrayToFilter.slice( this.state.index.start, this.state.index.end );
+    // initial display of 20.
+    // also sets up the manipulation array separate from initial product list (maintaining immutability on original product list)
+    this.setState( {
+      displayList: tempArray
+    });
+  }
+
+  showMore(){
+    let start = this.state.index.start;
+    let end = this.state.index.end;
+    let tempArray = this.state.displayList;
+    start += 20;
+    end += 20;
+    let newArray = tempArray.concat(this.state.productList.slice(start, end));
+    this.setState({
+      displayList: newArray,
+      index:{
+        start: start,
+        end: end
+      }
+    });
   }
 
   // do any data manipulations BEFORE sending to render. 
@@ -41,12 +75,13 @@ class ProductList extends Component {
   // send filtered array to render.
   // setup AZ and price filters.
   componentDidUpdate( prevProps, prevState ) {
-    window.scrollTo( 0, 500 )
+    window.scrollTo( 0, 100 )
   }
 
   componentDidMount() {
     // data update happens after initial mount of component, as per Reactdocs.
     // axios to manage get request
+    // axios also handles onload/ init() for data.
     // as per challenge ensures data is not stored locally
     // ensures data is most up to date version of data.
     axios.get( 'https://joefresh-marketing-dev.s3.amazonaws.com/developer-interview/full-list.json' )
@@ -54,6 +89,9 @@ class ProductList extends Component {
         this.setState( {
           productList: result.data.results
         });
+      })
+      .then(() => {
+        this.initialFilter( this.state.productList );
       });
   }
 
